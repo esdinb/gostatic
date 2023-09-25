cdc96491ad0af1c9da3eb30a78abdd5bb09b753a
Espen <Dittmann>
2023-09-25 07:11:33 +0200

    switch to html parsing for markdown output

--- pkg/markdown/writer.go
+++ pkg/markdown/writer.go
@@ -1,15 +1,15 @@
 package markdown
 
 import (
-    "unsafe"
-    "errors"
+	"errors"
+	"unsafe"
 
-    "github.com/yuin/goldmark/util"
-    "github.com/jbussdieker/golibxml"
+	"github.com/jbussdieker/golibxml"
+	"github.com/yuin/goldmark/util"
 )
 
 func NewTreeWriter(chunk string, filename string) TreeWriter {
-    return TreeWriter{golibxml.CreatePushParser(chunk, filename)}
+	return TreeWriter{golibxml.CreateHTMLPushParser(chunk, filename)}
 }
 
 // implements the html.Writer interface
@@ -17,40 +17,37 @@ type HtmlWriter struct {
 }
 
 func (w HtmlWriter) Write(writer util.BufWriter, source []byte) {
-    writer.Write(source)
+	writer.Write(source)
 }
 
 func (w HtmlWriter) RawWrite(writer util.BufWriter, source []byte) {
-    w.Write(writer, source)
+	w.Write(writer, source)
 }
 
 func (w HtmlWriter) SecureWrite(writer util.BufWriter, source []byte) {
-    w.Write(writer, source)
+	w.Write(writer, source)
 }
-
-
 
 // implements the io.Writer interface
 type TreeWriter struct {
-    context *golibxml.Parser
+	context *golibxml.HTMLParser
 }
 
 func (w TreeWriter) Terminate(chunk string) {
-    w.context.Terminate(chunk)
+	w.context.Terminate(chunk)
 }
 
 func (w TreeWriter) Document() *golibxml.Document {
-    return w.context.MyDoc()
+	return w.context.MyDoc()
 }
 
 func (w TreeWriter) Free() {
-    w.context.Free()
+	w.context.Free()
 }
 
 func (w *TreeWriter) Write(p []byte) (n int, err error) {
-    if res := w.context.ParseChunk(*(*string)(unsafe.Pointer(&p))); res != 0 {
-        return 0, errors.New("TreeWriter error")
-    }
-    return len(p), nil
+	if res := w.context.ParseChunk(*(*string)(unsafe.Pointer(&p))); res != 0 {
+		return 0, errors.New("TreeWriter error")
+	}
+	return len(p), nil
 }
-
